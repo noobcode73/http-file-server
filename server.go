f8bc91cf2517cf5357127989e72879b52fdf2286
Müller <35029280+muller2002@users.noreply.github.com>
2022-09-04 23:17:15 +0200

    Update server.go

--- server.go
+++ server.go
@@ -51,7 +51,7 @@ const directoryListingTemplateText = `
 	{{- range .Files }}
 	<tr>
 		{{ if (not .IsDir) }}
-		<td class=text><a href="{{ .URL.String }}">{{ .Name }}</a></td>
+		<td class=text><a href="{{ .URL.String }}">{{ .Name }}Hallo</a></td>
 		<td class=text><a href="{{ .URL.String }}" download>(Download)</a></td>
 		<td class=number>{{.Size.String }}</td>
 		<td class=number>({{ .Size | printf "%d" }})</td>
